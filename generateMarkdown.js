cd8861b91bd762a57c31ddc3de521f1e9b568049
ManHoff <achoffman91@gmail.com>
2023-01-25 15:07:32 -0500

    added license functions and writes file succesfuly

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,19 +1,52 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+//  Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license || license === "none") {
+    return "";
+  } else if (license === "MIT") {
+    return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+  } else if (license === "GPL") {
+    return "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
+  } else if (license === "Apache 2.0") {
+    return "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)";
+  } else if (license === "MPL-2.0") {
+    return "![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)";
+  }
+}
 
-// TODO: Create a function that returns the license link
+//  Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license || license === "none") {
+    return "";
+  } else if (license === "MIT") {
+    return "https://opensource.org/licenses/MIT";
+  } else if (license === "GPL") {
+    return "https://www.gnu.org/licenses/gpl-3.0";
+  } else if (license === "Apache v2.0") {
+    return "https://opensource.org/licenses/Apache-2.0";
+  } else if (license === "MPL-2.0") {
+    return "https://opensource.org/licenses/MPL-2.0";
+  }
+}
 
-// TODO: Create a function that returns the license section of README
+//  Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license || license === "none") {
+    return "";
+  }
+  return `## License
 
-// TODO: Create a function to generate markdown for README
+This project is licensed under the ${renderLicenseBadge(license)} license.
+
+Read more about license here: ${renderLicenseLink(license)}`;
+}
+
+// TODO: structure out read me 
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseSection(data.license)}
 `;
 }
 
--- index.js
+++ index.js
@@ -1,67 +1,74 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./generateMarkdown.js');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-      type: 'input',
-      name: 'ghusername',
-      message: 'What is your GitHub username?'
-      
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'What is your email address?'
-    },
-    {
-      type: 'input',
-      name: 'projectname',
-      message: 'What is your projects name?'
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Please write a short description of your project.'
-    },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'What kind of license should your project have?',
-      choices: ['MIT', 'GPL', 'Apache 2.0', 'MPL-2.0', 'CDDL-1.0','none'],
-      default: 'MIT'
-    },
-    {
-      type: 'input',
-      name: 'install',
-      message: 'What command should be run to install dependencies?'
-    },
-    {
-      type: 'input',
-      name: 'testcommand',
-      message: 'What command should be run to run tests?'
-    },
-    {
-      type: 'input',
-      name: 'whattoknow',
-      message: 'What does user need to know when using repo?'
-    },
-    {
-      type: 'input',
-      name: 'contributing',
-      message: 'What does user need to know about contributing to repo?'
-    }
-  ];
-
+  {
+    type: "input",
+    name: "ghusername",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is your projects name?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project.",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "GPL", "Apache 2.0", "MPL-2.0", "none"],
+    default: "MIT",
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "What command should be run to install dependencies?",
+  },
+  {
+    type: "input",
+    name: "testcommand",
+    message: "What command should be run to run tests?",
+  },
+  {
+    type: "input",
+    name: "whattoknow",
+    message: "What does user need to know when using repo?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What does user need to know about contributing to repo?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-  
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Your README.md file has been generated!");
+  });
+}
+
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
+  inquirer.prompt(questions).then((data) => {
+    writeToFile("README-TEST.md", generateMarkdown(data));
+  });
 }
 
 // Function call to initialize app
